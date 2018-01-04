aa1129fc08e6a73a23d2177a4bcd333da5610f6e
sebastianpiecowski <seba.piecowski@gmail.com>
2018-01-04 20:12:41 +0100

    fix

--- src/main/java/prs/models/PurposeModel.java
+++ src/main/java/prs/models/PurposeModel.java
@@ -2,10 +2,8 @@ package prs.models;
 
 import java.sql.Time;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
